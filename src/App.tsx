73b351a66950dd8588e6433d579ec13a52c59f74
Brandon <name.brandon.aguero@gmail.com>
2023-09-12 12:37:42 -0500

    Configuración inicial para desarrollar el proyecto 'Tic tac toe'

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+function App(): JSX.Element {
+  return (
+    <>
+      <button className="bg-sky-700 px-4 py-2 text-white hover:bg-sky-800 sm:px-8 sm:py-3">
+        press on me
+      </button>
+    </>
+  );
+}
+
+export default App;
